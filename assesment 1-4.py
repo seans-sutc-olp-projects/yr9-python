ed00c938e0795d571d611ca449b00510240f8ae3
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-02 10:00:38 +0000

    updated with lesson stuff

---
+++ assesment 1-4.py
@@ -0,0 +1,36 @@
+print ("Pratical Questions")
+divider = ('''
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////////////////////////////////////////////////////////////
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+''')
+print (divider)
+# question 1
+print ("Question 1:")
+print ("Hello World", "This is Python", "Sean Outram", 999,)
+print (divider)
+# question 2
+print ("Question 2")
+userName = ("PGriffin")
+firstName = ("Peter")
+currentMonth = ("March")
+dayOfTheMonth = (16)
+theDayOfTheWeek = ("Tuesday")
+print (userName, firstName, currentMonth, dayOfTheMonth,theDayOfTheWeek)
+print (divider)
+#question 3
+print ("Question 3")
+userName = input ("What Is Your Username? ")
+firstName = input ("What Is Your First Name? ")
+currentMonth = input ("What Is The Current Month? ")
+dayOfTheMonth = input ("What Is The Day Of The Month? ")
+theDayOfTheWeek = input ("What Is The Day Of The Week? ")
+print (userName, firstName, currentMonth, dayOfTheMonth,theDayOfTheWeek)
+print (divider)
+#question 4
+print ("Question 4")
+name = str("Peter")
+age = int(35)
+height = float(1.85)
+isMale = bool(True)
+print (name, age, height, isMale)
---
+++ lesson5.py
@@ -0,0 +1,43 @@
+'''
+###First Task###
+num1Input = input("First Number: ") 
+num2Input = input("Second Number: ")
+num3Input = input("Third Number: ")
+num1Int = int(num1Input)
+num2Int = int(num2Input)
+num3Int = int(num3Input)
+total = num1Int + num2Int * num3Int
+print (total)
+'''
+'''
+###Second Task###
+ans1 = 186 + 225
+ans2 = 133 - 895
+ans3 = 35 * 41
+ans4 = 12345 / 5
+ans5 = (5 + 5 - (5 * 5))/5
+print(ans1)
+print(ans2)
+print(ans3)
+print(ans4)
+print(ans5)
+'''
+'''
+###Task Threee###
+#first num input
+num1Input = input("First Number: ")
+#second num input
+num2Input = input("Second Number: ")
+#third num input
+num3Input = input("Third Number: ")
+#ints num 1
+num1Int = int(num1Input)
+#ints num 2 
+num2Int = int(num2Input)
+#ints num 3
+num3Int = int(num3Input)
+#adds num 1, num 2,num 3
+total = num1Int + num2Int * num3Int
+#print answer
+print (total)
+'''
