2116bd71a9497c03d63a54fe68bf6d91acdaa6b6
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-16 10:05:04 +0000

    assessment 1-7

---
+++ assesment 1-7.py
@@ -0,0 +1,38 @@
+
+# Q1:
+print("Hello World")
+print(12345)
+# Q2:
+q2Username = "TCrews"
+q2DayOfTheWeek = "Thursday"
+# Q3:
+q2Username = input("What is your username? ")
+q2DayOfTheWeek = input("What is the day of the week? ")
+# Q4:
+q4Name = str("Terry")
+q4Age = int(42)
+q4Height = float(1.92)
+q5IsFemale = bool(False)
+# Q5:
+
+# This is a comment
+'''
+This is
+also a
+comment.
+'''
+
+# Q6:
+q6VariableA = 100
+q6VariableB = 99
+if q6VariableA == q6VariableB:
+    print("They are equal")
+if q6VariableA > q6VariableB:
+    print("A is greater")
+if q6VariableA != q6VariableB:
+    print("They are not the same")
+# Q7
+for x in range(10):
+    print("Hello World")
+for x in range(11):
+    print(x)
