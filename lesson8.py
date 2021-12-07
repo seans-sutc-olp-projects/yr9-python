dfec3f5e31e9b7dcd40ef4e33b3887154d0e6f3c
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-12-07 10:31:34 +0000

    Create lesson8.py

---
+++ lesson8.py
@@ -0,0 +1,30 @@
+# task 1
+t1q1 = ["John", "Bob", "Sam", "Joe"]
+print(t1q1[2])
+# task 2 question 1
+t2q1 = ["John", "Bob", "Sam", "Joe", "Jeff"]
+print(t2q1[3])
+# task 2 question 2
+t2q2 = ["Canada","USA","England"]
+print(t2q2[0])
+# task 2 question 3
+t2q3 = [1,2,3,4,5,6,7,8,9,10]
+print(t2q3[4])
+# task 3
+t3 = ["John", "Bob", "Sam", "Joe", "Jeff"]
+for x in t3:
+    print(x)
+#t4q1
+for x in t3:
+    print (x)
+#t4q2
+for x in t2q1:
+    print (x)
+#t4q3
+t4q3 = ["Red","Green","Yellow","Blue"]
+for x in t4q3:
+    if x == [0]:
+        print(x[0]
+    else if x == [3]:
+        print(x[3])
+    
