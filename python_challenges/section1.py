4fea28b124fe8902f332a4fca0b926e13fc620dd
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-03-01 10:31:23 +0000

    ..

---
+++ python_challenges/section1.py
@@ -0,0 +1,21 @@
+# Section1
+
+# Q1
+print('Q1')
+age = '13'
+height = '5'
+fullName = 'sean outram'
+likeCheese = 'true'
+
+# Q2
+print('Q2')
+print('A: My name is' + fullName+ 'and i am' + age + 'years old.')
+print('B:' + fullName + 'likes cheese?' + likeCheese)
+print('C: Despite not being' + height  + 'meters tall, i am actually'+ age + 'years old')
+
+# Q3
+print('Q3')
+age = int(input('Age: '))
+height = int(input('Height: '))
+fullName = str(input('Full Name: '))
+likeCheese = bool(input('Like Cheese:  '))
---
+++ python_challenges/section2.py
@@ -0,0 +1,39 @@
+# Section2
+
+# Q1
+print('Q1:')
+nameArray = ['Bob', 'Dave', 'Sam', 'Oliver', 'Ethan']
+
+# Q2
+print('Q2:')
+for i in nameArray:
+    print(i)
+
+# Q3
+print('Q3:')
+surNameArray = ['smith', 'smith', 'bob', 'bob', 'smith',]
+
+# Q4
+print('Q4:')
+for i in range(len(nameArray)):
+    print(nameArray[i])
+    print(surNameArray[i])
+
+# Q5
+print('Q5:')
+for i in range(26):
+    print(i)
+
+# Q6
+print('Q6:')
+for i in range(101):
+    if i % 2 != 0:
+        print(i)
+    else:
+        print('')
+
+# Q7
+print('Q7')
+arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+print(sorted(arr, reverse=True))
+
